5270732bc374fcd6b78304222eb9ae3e4ea528e6
Avet Darbinyan <avet2009gg@gmail.com>
2025-01-31 20:56:07 +0400

    homework 31.01.2025

---
+++ 23.01.2025/hasvic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+        int a = 4;
+        int b = 4
+        std::cout << a++<< std:: endl;
+        std::cout << ++b <<std:: endl;
+}
+~                                                                                                                                        
+~                                                                                                                                        
+~                                                                                                                                        
+~                                                                                                                                        
+~                                     
