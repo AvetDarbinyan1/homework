9d9c79701d3c4fb304632f93c308e49d2cbbaeca
Avet Darbinyan <avet2009gg@gmail.com>
2025-02-03 14:25:36 +0400

    homework

--- 23.01.2025/hasvic.cpp
+++ 23.01.2025/hasvic.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-int main(){
-        int a = 4;
-        int b = 4
-        std::cout << a++<< std:: endl;
-        std::cout << ++b <<std:: endl;
+
+
+int main() {
+    double num1 = 0;
+    double num2 = 0;
+    char operation;
+
+    std::cout << "nermuteq arajin tivy ";
+    std::cin >> num1;
+
+    std::cout << "nermutq nshany (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "nermuteq erkrord tivy ";
+    std::cin >> num2;
+
+    if (operation == '+') {
+	    std::cout << "ardiunq " << num1 + num2 << std::endl;
+    }
+    else if (operation == '-') {
+	    std::cout << "ardiunq " << num1 - num2 << std::endl;
+    }
+    else if (operation == '*') {
+	    std::cout << "ardiunq " << num1 * num2 << std::endl;
+    }
+    else if (operation == '/') {
+        if (num2 != 0) {
+		std::cout << "ardiunq" << num1 / num2 << std::endl;
+        } else {
+		std::cout << "ardiunq" << std::endl;
+        }
+    }
+   
+
+    return 0;
 }
-~                                                                                                                                        
-~                                                                                                                                        
-~                                                                                                                                        
-~                                                                                                                                        
-~                                     
+
