168a546141516fc0b5ffdea3234ce05894191d6c
Avet Darbinyan <avet2009gg@gmail.com>
2025-02-09 13:18:38 +0400

    homework

--- 31.01.2025/oddevennuber.cpp
+++ 31.01.2025/oddevennuber.cpp
@@ -8,11 +8,19 @@ int main(){
         std::cin>>a;
         a = a % 2;
 
+<<<<<<< HEAD
         if(a == 0){
                 std::cout<<"the number is even"<<std::endl;
         }else{
         std::cout<<"the number is odd"<<std::endl;
         }
+=======
+	if(a == 0){
+		std::cout<<"the number is even"<<std::endl;
+	}else{
+	std::cout<<"the number is odd"<<std::endl;
+	}
+>>>>>>> 4f9dd3f (homework)
 }
 ~                                                                               
 ~                                                                               
