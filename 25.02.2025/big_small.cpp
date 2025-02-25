047201361fb937f54bb916bf9ff65e9ce1dbb781
Avet Darbinyan <avet2009gg@gmail.com>
2025-02-25 14:49:11 +0400

    .

---
+++ 25.02.2025/big_small.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+void big_small(){
+	int a = 0;
+	int b = 0;
+	std::cout<<"arajin tiv@";'\n';
+	std::cin>>a;
+	std::cout<<"erkrord tiv@";'\n';
+	std::cin>>b;
+	if(a > b){
+		std::cout<<a;'\n';
+	}else{
+		std::cout<<b;'\n';
+	}
+}
+
+int main(){
+
+	big_small();
+return(0);
+}
