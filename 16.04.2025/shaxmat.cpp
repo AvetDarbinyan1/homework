c3432a656f5fd97e868ea0a74bfbbca289e5554f
Avet Darbinyan <avet2009gg@gmail.com>
2025-04-17 12:11:02 +0400

    shaxmati xndir y avelacav

---
+++ 16.04.2025/shaxmat.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+std::string a = "⬜️";
+std::string b = "⬛️";
+int c = 0;
+int d = 0;
+void print(){
+	std::cout<<"enter qanak"<<std::endl;
+	std::cin>>c;
+	int mas[c][c];
+	for(int i = 0;i < c;i++){
+		std::cout<<" "<<std::endl;
+		for(int j = 0;j < c;j++){
+			if(d == 0){
+			std::cout<<a;
+			d += 1;
+			}else{
+			std::cout<<b;
+			d -= 1;
+			}
+	}
+}
+}
+int main(){
+	print();		
+
+
+}
